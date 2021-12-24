0a875814eaaf2f40509e5affe0735de490c08775
haroldocruz <msom.info@gmail.com>
2021-12-23 23:31:20 -0300

    process: component; template

--- src/app/features/process/process-view/process-view.component.ts
+++ src/app/features/process/process-view/process-view.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProcessViewComponent implements OnInit {
 
+  timestamp = new Date().getTime()
+
   constructor() { }
 
   ngOnInit(): void {
