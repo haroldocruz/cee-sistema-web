5814ec0f5c49c990b862506a95f8dbb9da0ace00
haroldocruz <msom.info@gmail.com>
2022-01-10 18:44:19 -0300

    process: spec

--- src/app/features/process/process-form/process-form.component.spec.ts
+++ src/app/features/process/process-form/process-form.component.spec.ts
@@ -1,4 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, getTestBed, TestBed } from '@angular/core/testing';
+import { BsModalRef, BsModalService, ModalModule } from 'ngx-bootstrap/modal';
 
 import { ProcessFormComponent } from './process-form.component';
 
@@ -6,20 +7,27 @@ describe('ProcessFormComponent', () => {
   let component: ProcessFormComponent;
   let fixture: ComponentFixture<ProcessFormComponent>;
 
+  let bsModalService: BsModalService;
+  let bsModalRef: BsModalRef;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProcessFormComponent ]
+      declarations: [ ProcessFormComponent ],
+      imports: [ ModalModule.forRoot() ],
+      providers: [ BsModalService, BsModalRef ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    // bsModalService = getTestBed().get(BsModalService);
+    // bsModalRef = getTestBed().get(BsModalRef);
     fixture = TestBed.createComponent(ProcessFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
 });
