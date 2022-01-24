fa4523746982eea6ac8b730bce588d8787e5d05b
haroldocruz <msom.info@gmail.com>
2022-01-24 19:18:26 -0300

    app: change configs to remove error message related the initial exceeded maximum budget

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,37 @@
+
+import pack from '../../package.json';
 export const environment = {
   production: true
 };
+
+const api = {
+  // url: "api.cee.sigilo.digital"
+  // url: "https://cee-sistema-api-service-hxwfjtgxqq-uc.a.run.app"
+  // url: "http://192.168.137.1:3000"
+  // url: "http://10.2.0.90:3000"
+  url: "http://localhost:3000"
+}
+
+const app = {
+  version: pack.version
+  // version: "0.0.4b"
+}
+
+const user = {
+  defaultPassword: "1234567890"
+}
+
+const contact = {
+  phone: {
+      ddi: 55,
+      ddd: 63,
+      number: 988887777
+  }
+}
+
+export const ENV = {
+  api,
+  app,
+  user,
+  contact
+}
