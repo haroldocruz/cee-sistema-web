b9db9624b09126d49aba9bcf2900740befb10e45
Haroldo Cruz <haroldo.cruz@ikhon.com.br>
2023-03-19 13:31:02 -0300

    feature(avaliadores): novo modulo; routing
    - adicionado novo módulo com roteamento

---
+++ src/app/features/avaliadores/avaliadores.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AvaliadoresRoutingModule } from './avaliadores-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AvaliadoresRoutingModule
+  ]
+})
+export class AvaliadoresModule { }
